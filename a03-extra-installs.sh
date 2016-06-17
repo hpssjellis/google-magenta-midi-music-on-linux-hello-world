865ce9b9b5660642bbf10feb918824ba9927c047
Jeremy Ellis <keyfreemusic@gmail.com>
2016-06-17 09:51:23 -0700

    Create a03-extra-installs.sh

---
+++ a03-extra-installs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# installs git and timidity if you don't already have it
+
+
+# if you don't have git try
+
+echo "Installing Git"
+sudo apt-get install git
+
+
+
+
+# if you don't have a midi player try 
+
+echo "Installing timidity"
+sudo apt-get install timidity
+
+echo "run timidity in the /tmp/basic_rnn_generated with the command"
+cd /tmp/basic_rnn_generated
+echo "timidity basic_rnn_sample_0.mid"
+echo "I prefer copying the files to google drive and using iCloud to play them"
+echo "Have not yet found a good midi editor to create and clip midi files"
+
+
