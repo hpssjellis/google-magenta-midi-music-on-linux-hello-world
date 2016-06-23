0af97f1f708d0b7982ac0d5ac3d9106b1be3348c
Jeremy Ellis <keyfreemusic@gmail.com>
2016-06-23 09:33:42 -0700

    Update a03-extra-installs.sh

--- a03-extra-installs.sh
+++ a03-extra-installs.sh
@@ -15,11 +15,17 @@ sudo apt-get install git
 
 echo "Installing timidity"
 sudo apt-get install timidity
+sudo apt-get install lame
+
+# may need to use 
+#sudo apt-get install freepats timidity timidity-interfaces-extra
 
 echo "run timidity in the /tmp/basic_rnn_generated with the command"
 cd /tmp/basic_rnn_generated
 echo "timidity basic_rnn_sample_0.mid"
 echo "I prefer copying the files to google drive and using iCloud to play them"
 echo "Have not yet found a good midi editor to create and clip midi files"
+echo "To convert mid to mps run"
+echo "timidity basic_rnn_sample_0.mid -Ow -o - | lame - -b 64 a01out.mp3"
 
 
