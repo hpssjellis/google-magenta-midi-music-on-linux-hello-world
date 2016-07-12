b0e05d844c5d6fa060c3d6b38a277dc2e4c8989f
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-12 14:01:13 -0700

    added resetmagenta

---
+++ resetmagenta.sh
@@ -0,0 +1,35 @@
+#!/bin/bash  
+
+
+rm -rf /home/ben/mytemp/google-magenta-midi-music-on-linux-hello-world
+
+
+
+#rm -rf /tmp/basic_rnn/run1/train
+#rm /tmp/basic_rnn/run1/checkpoint
+#rm /tmp/basic_rnn/run1/train-200
+#rm /tmp/basic_rnn/run1/train-200.meta
+
+
+rm -rf /tmp/basic_rnn
+rm -rf /tmp/basic_rnn_generated
+
+
+rm -rf ~/mymagenta
+
+rm -rf ~/virtual-tf
+
+
+
+rm ~/tmp/training_melodies.tfrecord
+
+
+rm ~/tmp/evaluation_melodies.tfrecord
+
+
+rm ~/tmp/notesequences.tfrecord
+
+
+# don't forget to cleanup ~/.bashrc using nano
+# will hve several statements at the end of the file
+#nano ~/.bashrc
