324b25b75f6c6fa829c44f5d2e0d5371042f825c
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-17 08:15:41 -0700

    Update a01-rnn_basic.sh

--- a01-rnn_basic.sh
+++ a01-rnn_basic.sh
@@ -64,7 +64,7 @@ TRAIN_DATA=/tmp/training_melodies.tfrecord
 # primer.mid is provided as an example.
 PRIMER_PATH=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
 
-bazel run //magenta/models:basic_rnn_generate -- \
+bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
 --experiment_run_dir=/tmp/basic_rnn/run1 \
 --hparams='{"rnn_layer_sizes":[50]}' \
 --primer_midi=$PRIMER_PATH \
