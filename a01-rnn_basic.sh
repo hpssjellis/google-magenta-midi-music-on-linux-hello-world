80955899097bcf2c1e9910a74f6963c84a802f87
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-17 08:29:59 -0700

    Update a01-rnn_basic.sh

--- a01-rnn_basic.sh
+++ a01-rnn_basic.sh
@@ -70,7 +70,7 @@ PRIMER_PATH=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
 # we can use --primer_midi to prime our model with a melody stored in a MIDI file.
 
 #--primer_midi=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
-
+# --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
 
 
 
@@ -79,10 +79,10 @@ PRIMER_PATH=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
 bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
 --experiment_run_dir=/tmp/basic_rnn/run1 \
 --hparams='{"rnn_layer_sizes":[50]}' \
---primer_midi=$PRIMER_PATH \
 --output_dir=/tmp/basic_rnn_generated \
 --num_steps=640 \
---num_outputs=1
+--num_outputs=1 \
+--primer_midi=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
 
 
 echo ""
