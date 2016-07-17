654c334c6869e5624e164f60b7a9263f4458bb51
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-17 09:01:42 -0700

    Update a01-rnn_basic.sh

--- a01-rnn_basic.sh
+++ a01-rnn_basic.sh
@@ -58,19 +58,17 @@ TRAIN_DATA=/tmp/training_melodies.tfrecord
 
 
 
-
 # Provide a MIDI file to use as a primer for the generation.
 # The MIDI should just contain a short monophonic melody.
-# primer.mid is provided as an example.
-PRIMER_PATH=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
-
 
 # --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]" would prime the model with the first four notes of Twinkle Twinkle Little Star. 
 #Instead of using --primer_melody, 
 # we can use --primer_midi to prime our model with a melody stored in a MIDI file.
 
-#--primer_midi=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
+#--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
 # --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
+
+# or leave it blank and a random first note will be chosen.
 
 
 
@@ -82,11 +80,9 @@ bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
 --output_dir=/tmp/basic_rnn_generated \
 --num_steps=640 \
 --num_outputs=1 \
---primer_midi=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
+--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
 
 
 echo ""
-echo "Hopefully everything worked"
-echo "Look in the folder /tmp/basic_rnn_generated to see yout output"
-echo "I copy the files to Google Drive and use iCloud to play them, but try whatever works for you."
 
+echo "Look in the folder /tmp/basic_rnn_generated to see yout output"
