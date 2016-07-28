5f3a995f999282f6cc1fcd2e21df6c44599aa7ac
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-27 20:51:26 -0700

    Update a01-rnn_basic.sh

--- a01-rnn_basic.sh
+++ a01-rnn_basic.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+#when this file is saved as a01-rnn_basic.sh it can be run by typing (without the #) in the command line 
+#bash a01-rnn_basic.sh
+
+#First all the variables and what they do
+
+#location of all your midi files to be included in this run
 MIDI_DIRECTORY=~/mymagenta/magenta/magenta/testdata
 
-
 # TFRecord file that will contain NoteSequence protocol buffers.
-SEQUENCES_TFRECORD=/tmp/notesequences.tfrecord
-
-
-bazel run //magenta/scripts:convert_midi_dir_to_note_sequences -- \
---midi_dir=$MIDI_DIRECTORY \
---output_file=$SEQUENCES_TFRECORD \
---recursive
-
-
-
-
-# TFRecord file containing NoteSequence protocol buffers from convert_midi_dir_to_note_sequences.py.
 SEQUENCES_TFRECORD=/tmp/notesequences.tfrecord
 
 # Where training and evaluation datasets will be written.
@@ -31,6 +24,42 @@ EVAL_DATA=$DATASET_DIR/eval_melodies.tfrecord
 # normal is 0.10
 EVAL_RATIO=0
 
+# location of the latest run and checkpoints
+RUN_DIR=/tmp/basic_rnn/logdir/run1
+
+# Special parameters sent to train and generate your midi files
+HPARAMS='{"rnn_layer_sizes":[50]}'
+
+# The directory where the final generated set of midi files will be saved 
+OUTPUT_MIDI_DIR=/tmp/basic_rnn_generated
+
+
+# --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]" would prime the model with the first four notes of Twinkle Twinkle Little Star. 
+#Instead of using --primer_melody, 
+# we can use --primer_midi to prime our model with a melody stored in a MIDI file.
+
+#--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
+# --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
+
+# or leave it blank and a random first note will be chosen.
+
+
+# 
+$PRIMER_MIDI=~/mymagenta/magenta/magenta/models/shared/primer.mid
+
+
+#-----------------------------------------now the actual Bazel runs---------------------------------
+
+
+bazel run //magenta/scripts:convert_midi_dir_to_note_sequences -- \
+--midi_dir=$MIDI_DIRECTORY \
+--output_file=$SEQUENCES_TFRECORD \
+--recursive
+
+
+
+
+
 bazel run //magenta/models/basic_rnn:basic_rnn_create_dataset -- \
 --input=$SEQUENCES_TFRECORD \
 --output_dir=$DATASET_DIR \
@@ -45,7 +74,11 @@ bazel run //magenta/models/basic_rnn:basic_rnn_create_dataset -- \
 
 
 
-./bazel-bin/magenta/models/basic_rnn/basic_rnn_train --run_dir=/tmp/basic_rnn/logdir/run1 --sequence_example_file=$TRAIN_DATA --hparams='{"rnn_layer_sizes":[50]}' --num_training_steps=40
+./bazel-bin/magenta/models/basic_rnn/basic_rnn_train -- \
+--run_dir=$RUN_DIR \
+--sequence_example_file=$TRAIN_DATA \
+--hparams=$HPARAMS \
+--num_training_steps=40
 
 
 
@@ -68,12 +101,18 @@ bazel run //magenta/models/basic_rnn:basic_rnn_create_dataset -- \
 
 
 bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
---run_dir=/tmp/basic_rnn/logdir/run1 \
---hparams='{"rnn_layer_sizes":[50]}' \
---output_dir=/tmp/basic_rnn_generated \
+--run_dir=$RUN_DIR \
+--hparams=$HPARAMS \
+--output_dir=$OUTPUT_MIDI_DIR \
 --num_steps=640 \
---num_outputs=1 
+--num_outputs=1 \
+--temperature=1 \
+--bpm=120
+
+
 
 #\
-#--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
-
+#--primer_midi=$PRIMER_MIDI
+#or 
+# --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
+# or leave it blank to randomly generate a starting note.
