44f58abafee2464563eba95797ab59231fa32a73
Jeremy Ellis <keyfreemusic@gmail.com>
2016-07-27 23:40:44 -0700

    Update a01-rnn_basic-small.sh

--- a01-rnn_basic-small.sh
+++ a01-rnn_basic-small.sh
@@ -77,14 +77,20 @@ bazel run //magenta/models/basic_rnn:basic_rnn_create_dataset -- \
 
 
 
-#bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
-#--run_dir=$RUN_DIR \
-#--hparams=$HPARAMS \
-#--output_dir=/tmp/basic_rnn_generated \
-#--num_steps=640 \
-#--num_outputs=1 \
-#--temperature=1 \
-#--bpm=120
+#basic_rnn_generate Generates the output midi files
+#--run_dir=location of the latest run and checkpoints
+#--hparams=comma seperated list of hparameters
+#--output_dir Directory for all your midi files to be generated into
+#--num_steps Number of notes generated, more notes the longer the song is
+#--num_outputs Number of midi files generated
+#--temperature Randomness of outputted song. Default = 1, higher numbers for more random songs
+#--bpm  Beats per minute for the output songs. Default 120 bpm
+#--primer_midi A small midi file that starts each outputted midi fie
+#or
+# --primer_melody Strange format for writing small midi style songs
+#0r
+#leave blank for the first note to be randomly generated
+
 
 
 bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
@@ -100,6 +106,6 @@ bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
 
 #\
 #--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
-#or 
+
 # --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
-# or leave it blank to randomly generate a starting note.
+
